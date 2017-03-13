059960909e2f00cb95793603c0ad6408a29cf775
Vytautas <vytaut1as@gmail.com>
2017-03-13 13:10:53 +0200

    imput pridetas. IR is heloo uzklausia ir raso Hello ir jusu varda

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,12 @@
 
 
 public class HelloWorld {
+
+Imput imp = new Imput();
+
 	public static void main(String[] args) {
-		System.out.println("HELLO UGLY WORLD!");
-		System.out.println("The World is not ugly!");
+
+
+		System.out.println("HELLO " + imp.klaviatura());
 	}
 }
