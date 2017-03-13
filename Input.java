a77b526f65011577aa58ad609ae190a92c81fa59
Vytautas <vytaut1as@gmail.com>
2017-03-13 13:00:27 +0200

    Prideta imput.java

---
+++ Input.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		klaviatura();	
+
+
+	}
+		public static void klaviatura(){
+			System.out.println("iveskite betka");
+			String enter = sc.nextLine();
+			System.out.println(enter);
+			
+		}
+}
