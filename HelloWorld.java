929e4f492d0c517d43e2f77725a66d7d1c89a95b
Vytautas <vytaut1as@gmail.com>
2017-03-13 12:36:44 +0200

    HelloWorld.java programa papildomai spausdina The World is not ugly!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,8 @@
-package firstlesson;
 
-/**
- * @author zwaclaw
- *
- */
 
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HELLO UGLY WORLD!");
+		System.out.println("The World is not ugly!");
 	}
 }
