de0eb54c5248e3bf86377bad608c35137f7d6351
Vytautas <vytaut1as@gmail.com>
2017-03-13 13:18:32 +0200

    helloworld spauzdina data

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HELLO UGLY WORLD!");
 		System.out.println("The World is not ugly!");
+		System.out.println("data 2017 03 13");
 	}
 }
